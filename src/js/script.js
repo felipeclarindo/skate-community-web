5830717e058456d96638da3d1febb7bc214e2d02
felipeclarindo <fglpc2@gmail.com>
2024-06-09 14:13:21 -0300

    Adicionando javascript da pagina

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const buttonMenu = document.querySelector(".cabecalho__menu");
+const menu = document.querySelector(".menu-lateral");
+
+buttonMenu.addEventListener("click", () => {
+    menu.classList.toggle("menu-lateral--ativo")
+})
